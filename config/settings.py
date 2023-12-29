ffda70564aa66a25640b557ac5e221e192596f93
Adil S <forever@nzironeed.ru>
2023-12-30 01:34:17 +0500

    commit 3

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'DB_NAME': os.getenv('DB_NAME'),
+        'DB_PASSWORD': os.getenv('DB_PASSWORD'),
+        'DB_USER': os.getenv('DB_USER'),
     }
 }
 
--- tester_app/models.py
+++ tester_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Language(models.Model):
+    """
+    name
+    """
+    pass
+
+
+class Question(models.Model):
+    """
+    text
+    lang - foreign key
+    theme
+    type
+    """
+    pass
+
+
+class Answer(models.Model):
+    """
+    question - foreign key
+    text
+    is_right
+    """
+    pass
