ec58dec6e83a83ddc3be82457e11b8e340be5ffe
Adil S <forever@nzironeed.ru>
2024-01-14 01:35:44 +0500

    add MainView

--- tester_app/views.py
+++ tester_app/views.py
@@ -2,8 +2,14 @@ from django.views.generic import DetailView, ListView
 from tester_app.models import Question
 
 
+class MainView(ListView):
+    model = Question
+    template_name = 'tester_app/main.html'
+
+
 class QuestionListView(ListView):
     model = Question
+    template_name = 'tester_app/question_list.html'
 
 
 class QuestionDetailView(DetailView):
