3cdc91f6d345d0f76d9b4a06a30e6b0499f0ce73
Adil S <forever@nzironeed.ru>
2024-01-14 01:35:36 +0500

    add tester_app namespace

--- config/urls.py
+++ config/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from tester_app.views import MainView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MainView.as_view(), name='main'),
+    path('questions/', include('tester_app.urls', namespace='tester_app')),
 ]
