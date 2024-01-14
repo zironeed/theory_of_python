ba3e803bfaf82ee384f26eeda7150c2e86901d4a
Adil S <forever@nzironeed.ru>
2024-01-14 18:53:16 +0500

    add migrations and remake Category, Question models

--- tester_app/models.py
+++ tester_app/models.py
@@ -1,30 +1,48 @@
 from django.db import models
 
 
-class Themes(models.TextChoices):
-    oop = 'OOP'
-    python = "PYTHON's FEATURES"
-    sql = 'SQL'
-    solid = 'SOLID'
-    db = 'DATABASES'
-    rest = 'REST'
-    git = 'GIT'
-    fw_django = 'DJANGO'
-    
+class Category(models.Model):
+    """
+    about question, too
+    """
+    name = models.CharField(max_length=200, unique=True, verbose_name='category_name')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
 
 class Question(models.Model):
     """
     about question
     """
     text = models.TextField(max_length=200, unique=True, verbose_name='question_text')
-    theme = models.CharField(choices=Themes.choices, verbose_name='question_theme')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='question_theme',
+                                 related_name='question_category', unique=False, blank=True)
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
 
 
 class Answer(models.Model):
     """
     about right answer
     """
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='question')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='question',
+                                 related_name='question_answer')
     text = models.TextField(verbose_name='text')
     is_right = models.BooleanField(default=True, verbose_name='is_answer_right')
 
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Answer'
+        verbose_name_plural = 'Answers'
