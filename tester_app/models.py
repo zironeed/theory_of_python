1020913e0ea6d9c67af5e9656655606cc4a53ffb
Adil Salimgareev <forever@nzironeed.ru>
2024-08-27 21:55:23 +0500

    models: - added one common model - deleted all old models

--- tester_app/models.py
+++ tester_app/models.py
@@ -1,6 +1,22 @@
 from django.db import models
 
 
+class Question(models.Model):
+    """
+    Модель вопроса, включающая в себя текст вопроса, ответа и категорию
+    """
+    question = models.TextField(max_length=200, unique=True, verbose_name='question_text')
+    answer = models.TextField(verbose_name='text')
+    category = models.CharField(max_length=200, verbose_name='category_name')
+
+    def __str__(self):
+        return self.question
+
+    class Meta:
+        verbose_name = 'Question'
+        verbose_name_plural = 'Questions'
+
+
 class Category(models.Model):
     """
     about question, too
@@ -13,36 +29,3 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-
-
-class Question(models.Model):
-    """
-    about question
-    """
-    text = models.TextField(max_length=200, unique=True, verbose_name='question_text')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='question_theme',
-                                 related_name='question_category')
-
-    def __str__(self):
-        return self.text
-
-    class Meta:
-        verbose_name = 'Question'
-        verbose_name_plural = 'Questions'
-
-
-class Answer(models.Model):
-    """
-    about right answer
-    """
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, verbose_name='question',
-                                 related_name='question_answer')
-    text = models.TextField(verbose_name='text')
-    is_right = models.BooleanField(default=True, verbose_name='is_answer_right')
-
-    def __str__(self):
-        return self.text
-
-    class Meta:
-        verbose_name = 'Answer'
-        verbose_name_plural = 'Answers'
